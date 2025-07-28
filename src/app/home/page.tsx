affe0b18f46166234b1999d137e1dd65937a52ef
Siddharth Shenoy <sidshenoy2000@gmail.com>
2025-07-28 18:09:24 -0400

    Integrate API call to fetch restaurant data and add API call to fetch distinct grades

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,49 +2,73 @@
 
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
 import { useEffect, useState } from 'react';
 
 const columns: GridColDef[] = [
     { field: 'name', headerName: 'Name', width: 200, description: 'Restaurant Name' },
-    { field: 'address', headerName: 'Address', width: 300, description: 'Building and Street Address' },
+    { field: 'full_address', headerName: 'Address', width: 300, description: 'Building and Street Address' },
     { field: 'cuisine', headerName: 'Cuisine', width: 200, description: 'Cuisine offered' },
-    { field: 'grade', headerName: 'Grade', width: 90, description: 'Average grade based on most recent reviews', type: 'number' }
-];
-
-const rows = [
-    { id: 1, name: 'Riviera Caterer', address: '2780 Stillwell Avenue', cuisine: 'American', grade: 'A' },
-    { id: 2, name: 'Riviera Caterer', address: '2780 Stillwell Avenue', cuisine: 'American', grade: 'A' },
-    { id: 3, name: 'Riviera Caterer', address: '2780 Stillwell Avenue', cuisine: 'American', grade: 'A' }
+    { field: 'avg_grade', headerName: 'Grade', width: 90, description: 'Average grade based on most recent reviews', type: 'number' }
 ];
 
 const paginationModel = { page: 0, pageSize: 10 };
 
 export default function Home() {
+    const [pageNumber, setPageNumber] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [selectedGrade, setSelectedGrade] = useState('');
+    const [grades, setGrades] = useState([]);
     const [restaurants, setRestaurants] = useState([]);
 
     useEffect(() => {
+        fetch("http://localhost:8000/grades")
+            .then(response => response.json())
+            .then((data) => setGrades(data))
+            .catch((err) => console.log(err))
+
         fetch("http://localhost:8000/restaurants")
             .then(response => response.json())
-            .then((data) => {
-                console.log(data)
-            })
+            .then((data) => setRestaurants(data))
             .catch((err) => console.log(err))
     }, []);
 
+    function handleGradeSelect(event: SelectChangeEvent) {
+        setSelectedGrade(event.target.value as string);
+    }
+
     return (
-        <Paper sx={{ height: 400, width: '60%' }}>
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                initialState={{ pagination: { paginationModel } }}
-                pageSizeOptions={[5, 10]}
-                sx={{
-                    border: 0,
-                    '& .MuiDataGrid-columnHeaderTitle': { // Target the title element within the header
-                        fontWeight: 'bold',
-                    }
-                }}
-            />
-        </Paper>
+        <div>
+            <TextField id="pageNumber" label="Page Number" variant="outlined" />
+            <TextField id="limit" label="Number of Restaurants" variant="outlined" />
+            <InputLabel id="select-grade-label">Grade</InputLabel>
+            <Select
+                labelId="select-grade-label"
+                id="select-grade"
+                label="Grade"
+                onChange={handleGradeSelect}
+            >
+                {grades.map((grade: string, index: number) => <MenuItem value={grade}>{grade}</MenuItem>)}
+            </Select>
+            <Paper sx={{ height: 400, width: '60%' }}>
+                <DataGrid
+                    rows={restaurants}
+                    getRowId={(restaurant) => restaurant._id}
+                    columns={columns}
+                    initialState={{ pagination: { paginationModel } }}
+                    pageSizeOptions={[5, 10]}
+                    sx={{
+                        border: 0,
+                        '& .MuiDataGrid-columnHeaderTitle': { // Target the title element within the header
+                            fontWeight: 'bold',
+                        }
+                    }}
+                />
+            </Paper>
+        </div>
     );
 }
