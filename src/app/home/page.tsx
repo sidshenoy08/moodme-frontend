847ae95b21cc99faf9b73abf88005c6a751c90b6
Siddharth Shenoy <sidshenoy2000@gmail.com>
2025-07-29 12:10:01 -0400

    Allow users to filter restaurants based on name, address and cuisine

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -21,28 +21,31 @@ const paginationModel = { page: 0, pageSize: 10 };
 export default function Home() {
     const [pageNumber, setPageNumber] = useState(1);
     const [limit, setLimit] = useState(10);
-    const [selectedGrade, setSelectedGrade] = useState('');
-    const [grades, setGrades] = useState([]);
+    const [restaurantName, setRestaurantName] = useState('');
+    const [cuisine, setCuisine] = useState('');
+    const [address, setAddress] = useState('');
     const [restaurants, setRestaurants] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:8000/grades")
-            .then(response => response.json())
-            .then((data) => setGrades(data))
-            .catch((err) => console.log(err))
-    }, []);
+    // useEffect(() => {
+    //     fetch("http://localhost:8000/grades")
+    //         .then(response => response.json())
+    //         .then((data) => setGrades(data))
+    //         .catch((err) => console.log(err))
+    // }, []);
 
     useEffect(() => {
-        fetch(`http://localhost:8000/restaurants?page_number=${pageNumber}&limit=${limit}`)
+        fetch(`http://localhost:8000/restaurants?page_number=${pageNumber}&limit=${limit}&restaurant_name=${restaurantName}&address=${address}&cuisine=${cuisine}`)
             .then(response => response.json())
             .then((data) => setRestaurants(data))
             .catch((err) => console.log(err))
-    }, [pageNumber, limit, selectedGrade])
+    }, [pageNumber, limit, restaurantName, address, cuisine]);
 
     function handlePageChange(event: React.ChangeEvent<HTMLInputElement>) {
         let pno: number = parseInt(event.target.value, 10);
         if (!isNaN(pno)) {
             setPageNumber(pno);
+        } else {
+            setPageNumber(1);
         }
     }
 
@@ -50,18 +53,35 @@ export default function Home() {
         let lim: number = parseInt(event.target.value, 10);
         if (!isNaN(lim)) {
             setLimit(lim);
+        } else {
+            setLimit(10);
         }
     }
 
-    function handleGradeSelect(event: SelectChangeEvent) {
-        setSelectedGrade(event.target.value as string);
+    function handleRestaurantNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setRestaurantName(event.target.value as string);
     }
+
+    function handleCuisineChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setCuisine(event.target.value as string);
+    }
+
+    function handleAddressChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setAddress(event.target.value as string);
+    }
+
+    // function handleGradeSelect(event: SelectChangeEvent) {
+    //     setSelectedGrade(event.target.value as string);
+    // }
 
     return (
         <div>
             <TextField id="pageNumber" label="Page Number" variant="outlined" onChange={handlePageChange} />
             <TextField id="limit" label="Number of Restaurants" variant="outlined" onChange={handleLimitChange} />
-            <InputLabel id="select-grade-label">Grade</InputLabel>
+            <TextField id="restaurantName" label="Restaurant Name" variant="outlined" onChange={handleRestaurantNameChange} />
+            <TextField id="address" label="Address" variant="outlined" onChange={handleAddressChange} />
+            <TextField id="cuisine" label="Cuisine" variant="outlined" onChange={handleCuisineChange} />
+            {/* <InputLabel id="select-grade-label">Grade</InputLabel>
             <Select
                 labelId="select-grade-label"
                 id="select-grade"
@@ -70,14 +90,14 @@ export default function Home() {
                 onChange={handleGradeSelect}
             >
                 {grades.map((grade: string, index: number) => <MenuItem value={grade}>{grade}</MenuItem>)}
-            </Select>
+            </Select> */}
             <Paper sx={{ height: 400, width: '60%' }}>
                 <DataGrid
                     rows={restaurants}
                     getRowId={(restaurant) => restaurant._id}
                     columns={columns}
                     initialState={{ pagination: { paginationModel } }}
-                    pageSizeOptions={[5, 10]}
+                    pageSizeOptions={[5, 10, 50, 100]}
                     sx={{
                         border: 0,
                         '& .MuiDataGrid-columnHeaderTitle': { // Target the title element within the header
